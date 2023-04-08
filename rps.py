689041bf653948bf9644aa61612c469d4d663197
whyiseveryusernametak <bluesnek81@gmail.com>
2023-04-09 05:25:29 +0800

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,16 +1,38 @@
 import random
 print('rps')
-
+print('to quit type quit')
 while True:
     inp=input('rock paper or scissor: ')
     n=random.randint(1,3)
-    inp=inp.lower
-    try:
-        if inp=='rock':
-            if n==1:
+    inp=inp.lower()
+    if inp=='rock':
+        if n==1:
                 print('draw')
-            if
+        if n==2:
+            print('you lose')
+        if n==3:
+            print('you win')
+        print('now we start again')
+    elif inp=='paper':
+        if n==1:
+            print('you win')
+        if n==2:
+            print('draw')
 
+        if n==3:
+            print('you lose')
+        print('now we start again')
+    elif inp=='scissor':
+        if n==1:
+            print('you lose')
+        if n==2:
+            print('you win')
 
-    except Exception as ex:
+        if n==3:
+            print('draw')
+        print('now we start again')
+    elif inp=='quit':
+        break
+    else:
         print('enter a valid answer')
+print('you quit the game')
